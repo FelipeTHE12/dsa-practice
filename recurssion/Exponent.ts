02682bdeceee8df16534fef49af9615f18d4b77f
FelipeTHE12 <67246449+FelipeTHE12@users.noreply.github.com>
2025-01-18 02:28:22 -0300

    feat(RECURSSION): Exponent function

---
+++ recurssion/Exponent.ts
@@ -0,0 +1,11 @@
+function myCustomMathPow(base: number, exponent: number): number {
+    if (exponent === 0) {
+        return 1;
+    }
+
+    return base * myCustomMathPow(base, exponent - 1);
+}
+
+console.log(myCustomMathPow(2, 0)) // 1
+console.log(myCustomMathPow(2, 2)) // 4
+console.log(myCustomMathPow(2, 4)) // 16
